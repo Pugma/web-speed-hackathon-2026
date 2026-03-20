479e5db75638432e510648ee2f69ab6ef8c94916
Pugma <132571355+Pugma@users.noreply.github.com>
2026-03-20 14:57:15 +0900

    fix(datetime): use `Intl` namespace instead of `moment.js`

--- application/client/src/components/direct_message/DirectMessagePage.tsx
+++ application/client/src/components/direct_message/DirectMessagePage.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import moment from "moment";
+import { formatHM } from "@web-speed-hackathon-2026/client/src/utils/date_format";
 import {
   ChangeEvent,
   useCallback,
@@ -142,7 +142,7 @@ export const DirectMessagePage = ({
                 </p>
                 <div className="flex gap-1 text-xs">
                   <time dateTime={message.createdAt}>
-                    {moment(message.createdAt).locale("ja").format("HH:mm")}
+                    {formatHM(message.createdAt)}
                   </time>
                   {isActiveUserSend && message.isRead && (
                     <span className="text-cax-text-muted">既読</span>
