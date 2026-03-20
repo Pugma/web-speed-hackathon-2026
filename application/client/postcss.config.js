a3042fa7339f768c3fa7318399760bf03c09865c
Pugma <132571355+Pugma@users.noreply.github.com>
2026-03-20 11:13:10 +0900

    fix(tailwind): install `taiwindcss` `@tailwindcss/postcss` & add plugin settings

--- application/client/postcss.config.js
+++ application/client/postcss.config.js
@@ -4,6 +4,7 @@ const postcssPresetEnv = require("postcss-preset-env");
 module.exports = {
   plugins: [
     postcssImport(),
+    "@tailwindcss/postcss",
     postcssPresetEnv({
       stage: 3,
     }),
