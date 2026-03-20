ba0cd44f25cdbf127d306d2134cc1023398e8c80
Pugma <132571355+Pugma@users.noreply.github.com>
2026-03-20 16:34:35 +0900

    fix(server): ensure maxAge is set for static file serving & reuse TCP connections

--- application/server/src/app.ts
+++ application/server/src/app.ts
@@ -15,13 +15,13 @@ app.use(sessionMiddleware);
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ limit: "10mb" }));
 
-app.use((_req, res, next) => {
-  res.header({
-    "Cache-Control": "max-age=0",
-    Connection: "close",
-  });
-  return next();
-});
+app.use(
+  "/api/v1",
+  (_req, res, next) => {
+    res.header({ "Cache-Control": "max-age=0" });
+    return next();
+  },
+  apiRouter,
+);
 
-app.use("/api/v1", apiRouter);
 app.use(staticRouter);
