73e6266a1a9b96c47f81331e42b9b3dabbb677a0
Pugma <pugmatech@gmail.com>
2026-03-20 15:19:53 +0900

    perf(routes): lazy-load all routes except home

--- application/client/src/containers/AppContainer.tsx
+++ application/client/src/containers/AppContainer.tsx
@@ -1,20 +1,58 @@
-import { useCallback, useEffect, useId, useState } from "react";
+import { lazy, Suspense, useCallback, useEffect, useId, useState } from "react";
 import { Helmet, HelmetProvider } from "react-helmet";
 import { Route, Routes, useLocation, useNavigate } from "react-router";
 
 import { AppPage } from "@web-speed-hackathon-2026/client/src/components/application/AppPage";
 import { AuthModalContainer } from "@web-speed-hackathon-2026/client/src/containers/AuthModalContainer";
-import { CrokContainer } from "@web-speed-hackathon-2026/client/src/containers/CrokContainer";
-import { DirectMessageContainer } from "@web-speed-hackathon-2026/client/src/containers/DirectMessageContainer";
-import { DirectMessageListContainer } from "@web-speed-hackathon-2026/client/src/containers/DirectMessageListContainer";
 import { NewPostModalContainer } from "@web-speed-hackathon-2026/client/src/containers/NewPostModalContainer";
-import { NotFoundContainer } from "@web-speed-hackathon-2026/client/src/containers/NotFoundContainer";
-import { PostContainer } from "@web-speed-hackathon-2026/client/src/containers/PostContainer";
-import { SearchContainer } from "@web-speed-hackathon-2026/client/src/containers/SearchContainer";
-import { TermContainer } from "@web-speed-hackathon-2026/client/src/containers/TermContainer";
 import { TimelineContainer } from "@web-speed-hackathon-2026/client/src/containers/TimelineContainer";
-import { UserProfileContainer } from "@web-speed-hackathon-2026/client/src/containers/UserProfileContainer";
-import { fetchJSON, sendJSON } from "@web-speed-hackathon-2026/client/src/utils/fetchers";
+import {
+  fetchJSON,
+  sendJSON,
+} from "@web-speed-hackathon-2026/client/src/utils/fetchers";
+
+const lazyLoad = <T extends Record<string, React.ComponentType<any>>>(
+  factory: () => Promise<T>,
+  name: keyof T,
+) => lazy(() => factory().then((m) => ({ default: m[name] })));
+
+const CrokContainer = lazyLoad(
+  () => import("@web-speed-hackathon-2026/client/src/containers/CrokContainer"),
+  "CrokContainer",
+);
+const DirectMessageContainer = lazyLoad(
+  () =>
+    import("@web-speed-hackathon-2026/client/src/containers/DirectMessageContainer"),
+  "DirectMessageContainer",
+);
+const DirectMessageListContainer = lazyLoad(
+  () =>
+    import("@web-speed-hackathon-2026/client/src/containers/DirectMessageListContainer"),
+  "DirectMessageListContainer",
+);
+const NotFoundContainer = lazyLoad(
+  () =>
+    import("@web-speed-hackathon-2026/client/src/containers/NotFoundContainer"),
+  "NotFoundContainer",
+);
+const PostContainer = lazyLoad(
+  () => import("@web-speed-hackathon-2026/client/src/containers/PostContainer"),
+  "PostContainer",
+);
+const SearchContainer = lazyLoad(
+  () =>
+    import("@web-speed-hackathon-2026/client/src/containers/SearchContainer"),
+  "SearchContainer",
+);
+const TermContainer = lazyLoad(
+  () => import("@web-speed-hackathon-2026/client/src/containers/TermContainer"),
+  "TermContainer",
+);
+const UserProfileContainer = lazyLoad(
+  () =>
+    import("@web-speed-hackathon-2026/client/src/containers/UserProfileContainer"),
+  "UserProfileContainer",
+);
 
 export const AppContainer = () => {
   const { pathname } = useLocation();
@@ -65,23 +103,77 @@ export const AppContainer = () => {
           <Route element={<TimelineContainer />} path="/" />
           <Route
             element={
-              <DirectMessageListContainer activeUser={activeUser} authModalId={authModalId} />
+              <Suspense>
+                <DirectMessageListContainer
+                  activeUser={activeUser}
+                  authModalId={authModalId}
+                />
+              </Suspense>
             }
             path="/dm"
           />
           <Route
-            element={<DirectMessageContainer activeUser={activeUser} authModalId={authModalId} />}
+            element={
+              <Suspense>
+                <DirectMessageContainer
+                  activeUser={activeUser}
+                  authModalId={authModalId}
+                />
+              </Suspense>
+            }
             path="/dm/:conversationId"
           />
-          <Route element={<SearchContainer />} path="/search" />
-          <Route element={<UserProfileContainer />} path="/users/:username" />
-          <Route element={<PostContainer />} path="/posts/:postId" />
-          <Route element={<TermContainer />} path="/terms" />
           <Route
-            element={<CrokContainer activeUser={activeUser} authModalId={authModalId} />}
+            element={
+              <Suspense>
+                <SearchContainer />
+              </Suspense>
+            }
+            path="/search"
+          />
+          <Route
+            element={
+              <Suspense>
+                <UserProfileContainer />
+              </Suspense>
+            }
+            path="/users/:username"
+          />
+          <Route
+            element={
+              <Suspense>
+                <PostContainer />
+              </Suspense>
+            }
+            path="/posts/:postId"
+          />
+          <Route
+            element={
+              <Suspense>
+                <TermContainer />
+              </Suspense>
+            }
+            path="/terms"
+          />
+          <Route
+            element={
+              <Suspense>
+                <CrokContainer
+                  activeUser={activeUser}
+                  authModalId={authModalId}
+                />
+              </Suspense>
+            }
             path="/crok"
           />
-          <Route element={<NotFoundContainer />} path="*" />
+          <Route
+            element={
+              <Suspense>
+                <NotFoundContainer />
+              </Suspense>
+            }
+            path="*"
+          />
         </Routes>
       </AppPage>
 
