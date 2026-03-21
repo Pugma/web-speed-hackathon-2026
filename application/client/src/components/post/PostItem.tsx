1125051a1402be1140113163aacb21be526a115a
Pugma <132571355+Pugma@users.noreply.github.com>
2026-03-21 12:43:02 +0900

    fix(post-item): add `isFirst` arg

--- application/client/src/components/post/PostItem.tsx
+++ application/client/src/components/post/PostItem.tsx
@@ -52,17 +52,17 @@ export const PostItem = ({ post }: Props) => {
           </div>
           {post.images?.length > 0 ? (
             <div className="relative mt-2 w-full">
-              <ImageArea images={post.images} />
+              <ImageArea images={post.images} isFirst={true} />
             </div>
           ) : null}
           {post.movie ? (
             <div className="relative mt-2 w-full">
-              <MovieArea movie={post.movie} />
+              <MovieArea movie={post.movie} isFirst={true} />
             </div>
           ) : null}
           {post.sound ? (
             <div className="relative mt-2 w-full">
-              <SoundArea sound={post.sound} />
+              <SoundArea sound={post.sound} isFirst={true} />
             </div>
           ) : null}
           <p className="mt-2 text-sm sm:mt-4">
